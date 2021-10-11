ad2c15f35c162d3e45e11e335df159322fe00087
Lodis <lodis.perkins@aie.edu>
2021-10-11 16:55:46 -0500

    Added player class. Scene class now contains an actor array

--- Engine.cs
+++ Engine.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using MathLibrary;
 
 namespace MathForGames
 {
@@ -9,6 +11,7 @@ namespace MathForGames
         private static bool _applicationShouldClose = false;
         private static int _currentSceneIndex;
         private Scene[] _scenes = new Scene[0];
+        private static Icon[,] _buffer;
 
         /// <summary>
         /// Called to begin the application
@@ -34,7 +37,19 @@ namespace MathForGames
         /// </summary>
         private void Start()
         {
+            Scene scene = new Scene();
+            Actor actor = new Actor('P', 0, 0, "Actor1", ConsoleColor.Yellow);
+            Actor actor2 = new Actor('A', new MathLibrary.Vector2 { X = 10, Y = 10 }, "Actor2", ConsoleColor.Green);
+            Player player = new Player('@', 5, 5, 1, "Player", ConsoleColor.DarkMagenta);
+
+            scene.AddActor(actor);
+            scene.AddActor(actor2);
+            scene.AddActor(player);
+
+            _currentSceneIndex = AddScene(scene);
+
             _scenes[_currentSceneIndex].Start();
+
         }
 
         /// <summary>
@@ -50,7 +65,35 @@ namespace MathForGames
         /// </summary>
         private void Draw()
         {
+
+            Console.CursorVisible = false;
+
+            //Clear the stuff that was on the screen in the last frame
+            _buffer = new Icon[Console.WindowWidth, Console.WindowHeight - 1];
+
+            //Reset the cursor position to the top so the previous screen is drawn over
+            Console.SetCursorPosition(0, 0);
+
+            //Adds all actor icons to buffer
             _scenes[_currentSceneIndex].Draw();
+
+            //Iterate through buffer
+            for (int y = 0; y < _buffer.GetLength(1); y++)
+            {
+                for (int x = 0; x < _buffer.GetLength(0); x++)
+                {
+                    if (_buffer[x, y].Symbol == '\0')
+                        _buffer[x, y].Symbol = ' ';
+
+                    //Set console text color to be color of item at buffer
+                    Console.ForegroundColor = _buffer[x, y].Color;
+                    //Print the symbol of the item in the buffer
+                    Console.Write(_buffer[x, y].Symbol);
+                }
+
+                //Skip a line once the end of a row has been reached
+                Console.WriteLine();
+            }
         }
 
         /// <summary>
@@ -85,6 +128,40 @@ namespace MathForGames
 
             //Return the last index
             return _scenes.Length - 1;
+        }
+
+        /// <summary>
+        /// Gets the next key in the input stream
+        /// </summary>
+        /// <returns>The key that was pressed</returns>
+        public static ConsoleKey GetNextKey()
+        {
+            //If there is no key being pressed...
+            if (!Console.KeyAvailable)
+                //...return
+                return 0;
+
+            //Return the current key being pressed
+            return Console.ReadKey(true).Key;
+        }
+
+        /// <summary>
+        /// Adds the icon to the buffer to print to the screen in the next draw call
+        /// Prints the icon at the given position in the buffer
+        /// </summary>
+        /// <param name="icon">The icon to draw</param>
+        /// <param name="position">The position of the icon in the buffer</param>
+        /// <returns>False if the position is outside the bounds of the buffer</returns>
+        public static bool Render(Icon icon, Vector2 position)
+        {
+            //If the position is out of bounds...
+            if (position.X < 0 || position.X >= _buffer.GetLength(0) || position.Y < 0 || position.Y >= _buffer.GetLength(1))
+                //...return false
+                return false;
+
+            //Set the buffer at the index of the given position to be the icon
+            _buffer[(int)position.X, (int)position.Y] = icon;
+            return true;
         }
     }
 }
